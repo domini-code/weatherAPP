4cefb8db3c2f5d3123695ae1a2fc6af5bb4ebc2b
Bezael <bezael@gmail.com>
2022-01-22 20:48:48 +0100

    Add env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  openWeather: {
+    key: 'XXXXXXXXXXX',
+    url: 'https://api.openweathermap.org/data/2.5/'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  openWeather: {
+    key: 'XXXXXXXXXXX',
+    url: 'https://api.openweathermap.org/data/2.5/'
+  }
 };
 
 /*
